431cf80aaab2a60c497a728a5094b8e17943ae67
Brad Arsenault <brad@electricbrain.io>
2019-05-20 02:17:17 -0400

    more fixes to build scripts.

--- deployment/api_server/update_api_server.sh
+++ deployment/api_server/update_api_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed "s/REVISION_ID/$REVISION_ID/g;s/SWIFTLY_ENV/$SWIFTLY_ENV/g" deployment/api_server/api_server_deployment.yaml > api_server_deployment_$SWIFTLY_ENV.yaml
+sed "s/__REVISION_ID__/$REVISION_ID/g;s/__SWIFTLY_ENV__/$SWIFTLY_ENV/g" deployment/api_server/api_server_deployment.yaml > api_server_deployment_$SWIFTLY_ENV.yaml
 kubectl apply -f api_server_deployment_$SWIFTLY_ENV.yaml
 
 
--- deployment/api_server/update_demo_reset.sh
+++ deployment/api_server/update_demo_reset.sh
@@ -2,7 +2,7 @@
 
 if [ $SWIFTLY_ENV=testing ] ;
     then
-    sed "s/REVISION_ID/$REVISION_ID/g;s/SWIFTLY_ENV/$SWIFTLY_ENV/g" deployment/api_server/demo_reset_job.yaml > demo_reset_job_$SWIFTLY_ENV.yaml
+    sed "s/__REVISION_ID__/$REVISION_ID/g;s/__SWIFTLY_ENV__/$SWIFTLY_ENV/g" deployment/api_server/demo_reset_job.yaml > demo_reset_job_$SWIFTLY_ENV.yaml
     kubectl apply -f demo_reset_job_$SWIFTLY_ENV.yaml
 fi
 
