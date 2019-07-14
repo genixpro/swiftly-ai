834a2b92fc24d8285a61a6ea286410567a69a18a
Bradley Arsenault <brad@swiftlyai.com>
2019-07-14 13:56:29 -0400

    Fix to cap rate comp summary.

--- client/src/word_documents/comparable_sales_summaries.js
+++ client/src/word_documents/comparable_sales_summaries.js
@@ -41,9 +41,9 @@ class ComparableSalesSummaries extends React.Component {
 
                         if (compIndex % 2 === 0)
                         {
-                            return <table>
+                            return <table key={compIndex}>
                                 <tbody>
-                                        <tr key={compIndex}>
+                                        <tr>
                                         {imgElem}
                                         {descriptionElem}
                                     </tr>
@@ -52,9 +52,9 @@ class ComparableSalesSummaries extends React.Component {
                         }
                         else
                         {
-                            return <table>
+                            return <table key={compIndex}>
                             <tbody>
-                            <tr key={compIndex}>
+                            <tr>
                                 {descriptionElem}
                                 {imgElem}
                             </tr>
